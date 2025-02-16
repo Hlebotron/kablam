93a68cb07d920cd2bccec5e9de5bac3571f394e9
Hlebotron <stabasov@gmail.com>
2025-02-16 18:18:40 +0100

    Made the server respond to requests IT'S ALIVE!!!!!1!

--- src/http.rs
+++ src/http.rs
@@ -1,6 +1,6 @@
 use std::{
     net::{ TcpListener, TcpStream, IpAddr, ToSocketAddrs },
-    io::{ Read, Write, },
+    io::{ Read, Write, BufReader, self, BufRead },
     time::Duration,
     thread,
     default::Default,
@@ -9,6 +9,7 @@ use std::{
     convert::{ Infallible, From },
     rc::Rc,
 };
+use crate::{i, e, w}; 
 use HeaderKey::*;
 use HeaderContent::*;
 /*trait TryRead {
@@ -19,9 +20,11 @@ trait TryWrite {
     fn try_write(&mut self, buf: &mut [u8]) -> Result<Option<usize>>;
     fn write_timeout(&mut self, buf: &mut [u8], duration: Duration) -> Result<Option<usize>>;
 }*/
-//trait ToBytes {
-//    fn to_bytes(&self) -> Vec<u8>;
-//}
+const ADDRESS: &'static str = "192.168.1.133:6969";
+trait AsBytes {
+    fn as_bytes(&self) -> Vec<u8>;
+    //fn as_bytes_iter<T: Iterator>() -> T {}
+}
 impl Display for Header<'_> {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         write!(f, "{}: {}", self.0, self.1)
@@ -133,12 +136,71 @@ impl FromStr for HeaderKey {
 
 pub struct Server(TcpListener);
 pub struct WebSocket(TcpStream);
+pub struct HttpStream(TcpStream);
+impl HttpStream {
+    pub fn respond(&mut self, response: Response) -> std::io::Result<usize> {
+        let num = self.0.write(&response.as_bytes())?;
+        self.0.flush(); 
+        Ok(num)
+    }
+    pub fn get_requests(&mut self) -> std::io::Result<Vec<Request>> {
+        let mut lines = BufReader::new(&mut *self).lines();
+        let line1 = lines.next().expect("No line")?;
+        let mut method_path_version = line1.split(" ");
+        let method: Method = method_path_version
+            .next()
+            .unwrap()
+            .into();
+        i!("{}", method);
+        /*if len == 0 {
+            return Ok(Vec::new());
+        }*/
+        Ok(Vec::new())
+    }
+    pub fn try_get_requests(&self, timeout: Duration) -> std::io::Result<Vec<Request>> {
+        todo!()
+    }
+}
+impl Read for HttpStream {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        self.0.read(buf)
+    }
+}
 pub struct Request<'a> {
     method: Method,
-    url: Rc<str>,
+    path: Rc<str>,
     version: Version,
     headers: Vec<Header<'a>>,
     content: &'a [u8]
+}
+impl<'a> Request<'a> {
+    pub fn method(&self) -> Method {
+        self.method.clone()
+    }
+    pub fn path(&self) -> Rc<str> {
+        self.path.clone()
+    }
+    pub fn version(&self) -> Version {
+        self.version
+    }
+    pub fn headers(&self) -> &Vec<Header<'a>> {
+        &self.headers
+    }
+    pub fn headers_mut(&mut self) -> &mut Vec<Header<'a>> {
+        &mut self.headers
+    }
+    pub fn content(&self) -> &'a [u8] {
+        self.content
+    }
+    pub fn set_content(&mut self, content: &'a [u8]) {
+        let len = content.len();
+        self.content = content; 
+        for header in self.headers.iter_mut() {
+            if let ContentLength = header.0 {
+                header.1 = Number(len);
+            } 
+        }
+    }
 }
 pub struct Response<'a> {
     version: Version,
@@ -146,8 +208,50 @@ pub struct Response<'a> {
     headers: Vec<Header<'a>>,
     content: &'a [u8]
 }
+impl<'a> Response<'a> {
+    pub fn version(&self) -> Version {
+        self.version
+    }
+    pub fn http_code(&self) -> HttpCode {
+        self.code
+    }
+    pub fn headers(&mut self) -> &mut Vec<Header<'a>> {
+        &mut self.headers
+    }
+    pub fn content(&self) -> &'a [u8] {
+        self.content
+    }
+    pub fn set_content(&mut self, content: &'a [u8]) {
+        let len = content.len();
+        self.content = content; 
+        for header in self.headers.iter_mut() {
+            if let ContentLength = header.0 {
+                header.1 = Number(len);
+            } 
+        }
+    }
+    pub fn add_header(&mut self, header: Header<'a>) {
+        self.headers.push(header);
+    }
+    pub fn rm_header(&mut self, index: usize) {
+        self.headers.swap_remove(index);
+    }
+}
+impl<'a> From<Request<'a>> for Response<'a> {
+    fn from(value: Request<'a>) -> Self {
+        Response {
+            version: value.version,
+            code: HttpCode::OK,
+            headers: value.headers,
+            content: value.content
+        }
+    }
+}
+#[derive(Clone)]
+pub struct Header<'a>(HeaderKey, HeaderContent<'a>);
 #[derive(Clone, Copy)]
 pub struct Version(u8, u8);
+#[derive(Clone)]
 pub enum Method {
     Get,
     Post,
@@ -159,6 +263,41 @@ pub enum Method {
     Trace,
     Patch,
     Other(String)
+}
+impl From<&str> for Method {
+    fn from(value: &str) -> Method {
+        use Method::*;
+        match value {
+            "GET" => Get,
+            "POST" => Post,
+            "PUT" => Put,
+            "DELETE" => Delete,
+            "HEAD" => Head,
+            "CONNECT" => Connect,
+            "OPTIONS" => Options,
+            "TRACE" => Trace,
+            "PATCH" => Patch,
+            other => Other(other.to_string())
+        }
+    } 
+}
+impl Display for Method {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        use Method::*;
+        let write = match self {
+            Get => "GET",
+            Post => "POST",
+            Put => "PUT",
+            Delete => "DELETE",
+            Head => "HEAD",
+            Connect => "CONNECT",
+            Options => "OPTIONS",
+            Trace => "TRACE",
+            Patch => "PATCH",
+            Other(other) => &other
+        };
+        write!(f, "{}", write)
+    }
 }
 #[repr(u16)]
 #[derive(Clone, Copy)]
@@ -176,7 +315,7 @@ pub enum HttpCode {
     PartialContent,
     MultiStatus,
     AlreadyReported,
-    IMUsed,
+    IMUsed = 226,
     MultipleChoices = 300,
     MovedPermanently,
     Found,
@@ -198,13 +337,13 @@ pub enum HttpCode {
     Conflict,
     Gone,
     LengthRequired,
-    PreconditionFailed,
+    PreconditionFailed = 412,
     PayloadTooLarge,
     URITooLong,
     UnsupportedMediaType,
     RangeNotSatisfiable,
     ExpectationFailed,
-    MisdirectedRequest,
+    MisdirectedRequest = 421,
     UnprocessableRequest,
     Locked,
     FailedDependency,
@@ -212,8 +351,8 @@ pub enum HttpCode {
     UpgradeRequired,
     PreconditionRequired,
     TooManyRequests,
-    RequestHeaderFieldsTooLarge,
-    UnavailableForLegalReasons,
+    RequestHeaderFieldsTooLarge = 431,
+    UnavailableForLegalReasons = 451,
     InternalServerError = 500,
     NotImplemented,
     BadGateway,
@@ -221,8 +360,9 @@ pub enum HttpCode {
     GatewayTimeout,
     HttpVersionNotSupported,
     VariantAlsoNegotiates,
+    InsufficientStorage,
     LoopDetected,
-    NotExtended,
+    NotExtended = 510,
     NetworkAuthenticationRequired,
 }
 impl TryFrom<u16> for HttpCode {
@@ -243,7 +383,7 @@ impl TryFrom<u16> for HttpCode {
             206 => Ok(PartialContent),
             207 => Ok(MultiStatus),
             208 => Ok(AlreadyReported),
-            209 => Ok(IMUsed),
+            226 => Ok(IMUsed),
             300 => Ok(MultipleChoices),
             301 => Ok(MovedPermanently),
             302 => Ok(Found),
@@ -253,46 +393,116 @@ impl TryFrom<u16> for HttpCode {
             306 => Ok(SwitchProxy),
             307 => Ok(TemporaryRedirect),
             308 => Ok(PermanentRedirect),
-            500 => Ok(BadRequest),
-            501 => Ok(Unauthorized),
-            502 => Ok(PaymentRequired),
-            503 => Ok(Forbidden),
-            504 => Ok(NotFound),
-            505 => Ok(MethodNotAllowed),
-            506 => Ok(NotAcceptable),
-            507 => Ok(ProxyAuthenticationRequired),
-            508 => Ok(RequestTimeOut),
-            509 => Ok(Conflict),
-            510 => Ok(Gone),
-            511 => Ok(LengthRequired),
-            512 => Ok(PreconditionFailed),
-            513 => Ok(PayloadTooLarge),
-            514 => Ok(URITooLong),
-            515 => Ok(UnsupportedMediaType),
-            516 => Ok(RangeNotSatisfiable),
-            517 => Ok(ExpectationFailed),
-            518 => Ok(MisdirectedRequest),
-            519 => Ok(UnprocessableRequest),
-            520 => Ok(Locked),
-            521 => Ok(FailedDependency),
-            522 => Ok(TooEarly),
-            523 => Ok(UpgradeRequired),
-            524 => Ok(PreconditionRequired),
-            525 => Ok(TooManyRequests),
-            526 => Ok(RequestHeaderFieldsTooLarge),
-            527 => Ok(UnavailableForLegalReasons),
-            600 => Ok(InternalServerError),
-            601 => Ok(NotImplemented),
-            602 => Ok(BadGateway),
-            603 => Ok(ServiceUnavailable),
-            604 => Ok(GatewayTimeout),
-            605 => Ok(HttpVersionNotSupported),
-            606 => Ok(VariantAlsoNegotiates),
-            607 => Ok(LoopDetected),
-            608 => Ok(NotExtended),
-            609 => Ok(NetworkAuthenticationRequired),
+            400 => Ok(BadRequest),
+            401 => Ok(Unauthorized),
+            402 => Ok(PaymentRequired),
+            403 => Ok(Forbidden),
+            404 => Ok(NotFound),
+            405 => Ok(MethodNotAllowed),
+            406 => Ok(NotAcceptable),
+            407 => Ok(ProxyAuthenticationRequired),
+            408 => Ok(RequestTimeOut),
+            409 => Ok(Conflict),
+            410 => Ok(Gone),
+            411 => Ok(LengthRequired),
+            412 => Ok(PreconditionFailed),
+            413 => Ok(PayloadTooLarge),
+            414 => Ok(URITooLong),
+            415 => Ok(UnsupportedMediaType),
+            416 => Ok(RangeNotSatisfiable),
+            417 => Ok(ExpectationFailed),
+            421 => Ok(MisdirectedRequest),
+            422 => Ok(UnprocessableRequest),
+            423 => Ok(Locked),
+            424 => Ok(FailedDependency),
+            425 => Ok(TooEarly),
+            426 => Ok(UpgradeRequired),
+            428 => Ok(PreconditionRequired),
+            429 => Ok(TooManyRequests),
+            431 => Ok(RequestHeaderFieldsTooLarge),
+            451 => Ok(UnavailableForLegalReasons),
+            500 => Ok(InternalServerError),
+            501 => Ok(NotImplemented),
+            502 => Ok(BadGateway),
+            503 => Ok(ServiceUnavailable),
+            504 => Ok(GatewayTimeout),
+            505 => Ok(HttpVersionNotSupported),
+            506 => Ok(VariantAlsoNegotiates),
+            507 => Ok(InsufficientStorage),
+            508 => Ok(LoopDetected),
+            510 => Ok(NotExtended),
+            511 => Ok(NetworkAuthenticationRequired),
             _ => Err(())
         }
+    }
+}
+impl From<&HttpCode> for String {
+    fn from(value: &HttpCode) -> String {
+        use HttpCode::*;
+        match value {
+            Continue => "100 Continue",
+            SwitchingProtocols => "101 Switching Protocols",
+            Processing => "102 Processing",
+            EarlyHints => "103 Early Hints",
+            OK => "200 OK",
+            Created => "201 Created",
+            Accepted => "202 Accepted",
+            NonAuthorativeInformation => "203 Non-Authorative Information",
+            NoContent => "204 No Content",
+            ResetContent => "205 Reset Content",
+            PartialContent => "206 Partial Content",
+            MultiStatus => "207 Multi-Status",
+            AlreadyReported => "208 Already Reported",
+            IMUsed => "226 IM Used",
+            MultipleChoices => "300 Multiple Choices",
+            MovedPermanently => "301 Moved Permanently",
+            Found => "302 Found",
+            SeeOther => "303 See Other",
+            NotModified => "304 Not Modified",
+            UseProxy => "305 Use Proxy",
+            SwitchProxy => "306 Switch Proxy",
+            TemporaryRedirect => "307 Temporary Redirect",
+            PermanentRedirect => "308 Permanent Redirect",
+            BadRequest => "400 Bad Request",
+            Unauthorized => "401 Unauthorized",
+            PaymentRequired => "402 Payment Required",
+            Forbidden => "403 Forbidden",
+            NotFound => "404 Not Found",
+            MethodNotAllowed => "405 Method Not Allowed",
+            NotAcceptable => "406 Method Not Acceptable",
+            ProxyAuthenticationRequired => "407 Proxy Authentication Required",
+            RequestTimeOut => "408 Request Timeout",
+            Conflict => "409 Conflict",
+            Gone => "410 Gone",
+            LengthRequired => "411 Length Required",
+            PreconditionFailed => "412 Precondition Failed",
+            PayloadTooLarge => "413 Payload Too Large",
+            URITooLong => "414 URI Too Long",
+            UnsupportedMediaType => "415 Unsupported Media Type",
+            RangeNotSatisfiable => "416 Range Not Satisfiable",
+            ExpectationFailed => "417 Expectation Failed",
+            MisdirectedRequest => "421 Misdirected Request",
+            UnprocessableRequest => "422 Unprocessable Request",
+            Locked => "423 Locked",
+            FailedDependency => "424 Failed Dependency",
+            TooEarly => "425 Too Early",
+            UpgradeRequired => "426 Upgrade Required",
+            PreconditionRequired => "428 Precondition Required",
+            TooManyRequests => "429 Too Many Requests",
+            RequestHeaderFieldsTooLarge => "431 Request Header Fields Too Large",
+            UnavailableForLegalReasons => "451 Unavailable For Legal Reasons",
+            InternalServerError => "500 Internal Server Error",
+            NotImplemented => "501 Not Implemented",
+            BadGateway => "502 Bad Gateway",
+            ServiceUnavailable => "503 Service Unavailable",
+            GatewayTimeout => "504 Gateway Timeout",
+            HttpVersionNotSupported => "505 HTTP Version Not Supported",
+            VariantAlsoNegotiates => "506 Variant Also Negotiates",
+            InsufficientStorage => "507 Insufficient Storage",
+            LoopDetected => "508 Loop Detected",
+            NotExtended => "510 Not Extended",
+            NetworkAuthenticationRequired => "511 Network Authentication Required",
+        }.to_string()
     }
 }
 impl<'a> From<&'a str> for Response<'a> {
@@ -309,29 +519,44 @@ impl<'a> From<&'a [u8]> for Response<'a> {
         packet
     }
 }
-        //Response {
-        //    version: Version(1, 1),
-        //    code: 200u16.into(),
-        //    headers: Vec::new(),
-        //    content: &[69, 42]
-        //}
-impl Response<'_> {
-    pub fn as_bytes(&self) -> Vec<u8> {
+//pub struct Response<'a> {
+//    version: Version,
+//    code: HttpCode,
+//    headers: Vec<Header<'a>>,
+//    content: &'a [u8]
+//}
+impl AsBytes for Response<'_> {
+    fn as_bytes(&self) -> Vec<u8> {
         let version = self.version.as_bytes();
         let code = self.code.as_bytes();
-        unimplemented!()
+        let mut headers = Vec::<u8>::new();
+        for header in &self.headers {
+            headers.extend(header.as_bytes().into_iter());
+            headers.extend(b"\r\n");
+        }
+        let content = self.content;
+        let len: usize = &version.len() + &code.len() + &headers.len() + &content.len();
+        let mut vec: Vec<u8> = Vec::with_capacity(len);
+        vec.extend(version);
+        vec.extend(code);
+        vec.extend(b"\r\n");
+        vec.extend(headers);
+        vec.extend(b"\r\n");
+        vec.extend(content);
+        vec
     }
 }
-impl Version {
-    pub fn as_bytes(&self) -> Vec<u8> {
-        format!("HTTP/{}.{}", self.0, self.1).as_bytes().to_vec()
+impl AsBytes for Version {
+    fn as_bytes(&self) -> Vec<u8> {
+        format!("HTTP/{}.{} ", self.0, self.1).as_bytes().to_vec()
     }
 }
-impl HttpCode {
-    pub fn as_bytes(&self) -> Vec<u8> {
-        (*self as u16).to_be_bytes().to_vec()
+impl AsBytes for HttpCode {
+    fn as_bytes(&self) -> Vec<u8> {
+        String::from(self).as_bytes().to_vec()
     }
 }
+#[derive(Clone)]
 pub enum HeaderContent<'a> {
     Bytes(&'a [u8]),
     Text(String),
@@ -372,8 +597,8 @@ impl Display for HeaderContent<'_> {
         
     }
 }*/
-impl<'a> HeaderContent<'a> {
-    pub fn as_bytes(&self) -> Vec<u8> {
+impl<'a> AsBytes for HeaderContent<'a> {
+    fn as_bytes(&self) -> Vec<u8> {
         match self {
             Bytes(inner) => inner.to_vec(),
             Text(inner) => inner.as_bytes().to_vec(),
@@ -392,17 +617,26 @@ impl<'a> HeaderContent<'a> {
         }
     }
 }
-pub struct Header<'a>(HeaderKey, HeaderContent<'a>);
+impl AsBytes for Header<'_> {
+    fn as_bytes(&self) -> Vec<u8> {
+        self.to_string().as_bytes().to_vec()
+    }
+}
 impl Default for Response<'_> {
     fn default() -> Self {
+        let headers: Vec<Header> = vec![
+            Header(ContentType, Text("text/plain".to_string())),
+            Header(ContentLength, Number(3)),
+        ];
         Response {
             version: Version(1, 1),
-            code: 200u16.try_into().unwrap(),
-            headers: Vec::new(),
-            content: &[69, 42]
+            code: HttpCode::OK,
+            headers: headers,
+            content: b"pog" 
         }
     } 
 }
+#[derive(Clone)]
 pub enum HeaderKey {
     Host,
     Connection,
@@ -448,8 +682,7 @@ pub enum HeaderKey {
 impl Server {
     pub fn start_fn<T: ToSocketAddrs, F: Fn(TcpStream) + Send + Copy + 'static>(address: T, fun: F) -> std::io::Result<()> {
         let tcp_stream = TcpListener::bind(address)?; 
-        let server = Self(tcp_stream);
-        for res in server.0.incoming() {
+        for res in tcp_stream.incoming() {
             if let Ok(mut stream) = res {
                 thread::spawn(move || {
                     fun(stream);
@@ -459,20 +692,20 @@ impl Server {
         Ok(())
     }
     pub fn start<T: ToSocketAddrs>(address: T) -> std::io::Result<()> {
-        let fun = |mut stream: TcpStream| {
-            println!("pog");
-            let packet = Response::from("pog");
-            let _ = stream.write(b"pog");            
-            thread::sleep(Duration::from_secs(5));
-        };
-        Server::start_fn(address, fun)
+        Server::start_fn(address, handle_http)
     }
-    pub fn inner(&self) -> &TcpListener {
-        &self.0
-    }
-    pub fn inner_mut(&mut self) -> &mut TcpListener {
-        &mut self.0
-    }
+    //pub fn inner(&self) -> &TcpListener {
+    //    &self.0
+    //}
+    //pub fn inner_mut(&mut self) -> &mut TcpListener {
+    //    &mut self.0
+    //}
+}
+fn handle_http(tcp_stream: TcpStream) {
+    println!("Received Request");
+    let mut stream = HttpStream(tcp_stream);
+    let packet = Response::from("pog");
+    let _ = stream.respond(packet);            
 }
 #[cfg(test)]
 mod tests {
@@ -481,4 +714,15 @@ mod tests {
     fn start_server() {
         Server::start("192.168.1.133:6970").expect("Could not bind to address"); 
     }*/
+    #[test]
+    fn get_request() {
+        let mut stream = create_stream(); 
+        stream.get_requests().unwrap();
+        panic!("INTENDED")
+    }
+    fn create_stream() -> HttpStream {
+        let server = TcpListener::bind(ADDRESS).unwrap(); 
+        let stream = server.accept().ok().unzip().0.unwrap();
+        HttpStream(stream)
+    }
 }
